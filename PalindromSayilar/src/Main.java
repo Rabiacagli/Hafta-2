aeeeddbd344d4a95d58e9dc8661d0ec6218f3df4
Rabiacagli <rabiaacgl@gmail.com>
2023-12-19 22:26:32 +0300

    Palindrom Sayı Hesaplayan Program

--- PalindromSayilar/src/Main.java
+++ PalindromSayilar/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;   // kullanıcıdan veri almak için Scanner sınıfını kullanıyoruz
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static boolean isPalindrom(int number) {
+        // palindrom sayı değişkenlerini oluşturuyoruz
+
+        int temp = number, lastNumber, reverseNumber = 0;
+
+        // koşulları oluşturuyoruz
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            System.out.print("Bu sayı bir Palindrom sayıdır. ");
+            return true;
+        } else {
+            System.out.print("Bu sayı Palindrom sayı değildir. ");
+            return false;
+        }
     }
-}
+    public static void main(String[] args) {   // kullanıcıdan veri aldıktan sonra ekrana yazdırıyoruz
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı Giriniz: ");
+        int number = inp.nextInt();
+        System.out.print(isPalindrom(number));
+
+        }
+    }
