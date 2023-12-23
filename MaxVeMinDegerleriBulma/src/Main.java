c8ee52a44b5283f84327b01d1e4ae6fbe97b0123
Rabiacagli <rabiaacgl@gmail.com>
2023-12-23 16:46:32 +0300

    Girilen sayıya yakın küçük ve yakın büyük sayıyı bulma

--- MaxVeMinDegerleriBulma/src/Main.java
+++ MaxVeMinDegerleriBulma/src/Main.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Bir Sayı Giriniz: ");
+        System.out.print("Bir Sayı Giriniz: "); // kullanıcıdan veri alabilmek için Scanner sınıfını kullanıyoruz
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
 
-        int[] list = {2,56,9,n,15,302,-7,256};
-        Arrays.sort(list);
+        int[] list = {2,56,9,n,15,302,-7,256};  // dizinin içine 'n' sayısını yazıyoruz kullanıcıdan alacağız
+        Arrays.sort(list);                       // ve Arrays sınıfıyla tüm sayıları sıralıyoruz
         System.out.println(Arrays.toString(list));
 
-        for(int i = 0; i < list.length; i++){
+        for(int i = 0; i < list.length; i++){    // dizide 'n' sayısıyla bağlantılı küçük ve büyük olma koşulunu yazıyoruz.
             if (list[i] == n) {
 
                 int j = i - 1;
@@ -19,7 +19,7 @@ public class Main {
                 int yakinKucuk = list[j];
                 int yakinBuyuk = list[k];
 
-                System.out.println(" ");
+                System.out.println(" ");             // ve sonuçları ekrana yazdırıyoruz
                 System.out.println("Girilen Sayıya Yakın Küçük Sayı : " + yakinKucuk);
                 System.out.println("Girilen Sayıya Yakın Büyük Sayı : " + yakinBuyuk);
 
